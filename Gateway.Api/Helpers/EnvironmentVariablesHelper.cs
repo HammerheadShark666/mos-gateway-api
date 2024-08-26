67b55d5de47a22a3224bfcfc8f75136371a24588
John Miller <john.miller@unionstreet.co.uk>
2024-08-26 15:20:49 +0100

    Update routes to remove /api part

--- Gateway.Api/Helpers/EnvironmentVariablesHelper.cs
+++ Gateway.Api/Helpers/EnvironmentVariablesHelper.cs
@@ -4,5 +4,5 @@ public class EnvironmentVariablesHelper
 {
     public static string JwtIssuer = Environment.GetEnvironmentVariable(Constants.JwtIssuer);
     public static string JwtAudience = Environment.GetEnvironmentVariable(Constants.JwtAudience);
-    public static string JwtSymmetricSecurityKey = Environment.GetEnvironmentVariable(Constants.JwtSymmetricSecurityKey); 
+    public static string JwtSymmetricSecurityKey = Environment.GetEnvironmentVariable(Constants.JwtSymmetricSecurityKey);
 }
